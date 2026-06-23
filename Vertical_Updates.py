b50de17dfbca896553516a3a1e0ea41622431d20
letien2603-afk <letien2603@gmail.com>
2026-06-23 14:40:59 -0500

    Update Vertical_Updates.py

--- Vertical_Updates.py
+++ Vertical_Updates.py
@@ -115,13 +115,11 @@ def main():
             status_text.text("Processing ATF file and matching invoices...")
             df_atf['Original Invoice'] = df_atf['Invoice Number'].apply(clean_original_invoice)
             matched_atf = df_atf[df_atf['Original Invoice'].isin(original_invoices_memory)].copy()
-
-		matched_atf['SortKey'] = matched_atf['Invoice Number'].apply(parse_suffix_for_ranking)
+			matched_atf['SortKey'] = matched_atf['Invoice Number'].apply(parse_suffix_for_ranking)
 		# [CẬP NHẬT MỚI]: Dùng Temp_Amount để làm sạch trị tuyệt đối và gom nhóm 
-            	matched_atf['Temp_Amount'] = pd.to_numeric(matched_atf['Transaction Amount'], errors='coerce').abs()
-            	max_sort_keys = matched_atf.groupby(['Original Invoice', 'Temp_Amount'], dropna=False)['SortKey'].transform('max')
-            	matched_atf.drop(columns=['Temp_Amount'], inplace=True)
-            
+            matched_atf['Temp_Amount'] = pd.to_numeric(matched_atf['Transaction Amount'], errors='coerce').abs()
+            max_sort_keys = matched_atf.groupby(['Original Invoice', 'Temp_Amount'], dropna=False)['SortKey'].transform('max')
+            matched_atf.drop(columns=['Temp_Amount'], inplace=True)
             latest_atf = matched_atf[matched_atf['SortKey'] == max_sort_keys].copy()
 
 
