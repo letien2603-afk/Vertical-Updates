97846dd0a0b955147cb6233b258676a54f22c8c0
letien2603-afk <letien2603@gmail.com>
2026-06-23 14:56:51 -0500

    Update Vertical_Updates.py

--- Vertical_Updates.py
+++ Vertical_Updates.py
@@ -116,7 +116,7 @@ def main():
             df_atf['Original Invoice'] = df_atf['Invoice Number'].apply(clean_original_invoice)
             matched_atf = df_atf[df_atf['Original Invoice'].isin(original_invoices_memory)].copy()
 
-			matched_atf['SortKey'] = matched_atf['Invoice Number'].apply(parse_suffix_for_ranking)
+            matched_atf['SortKey'] = matched_atf['Invoice Number'].apply(parse_suffix_for_ranking)
 			matched_atf['Temp_Amount'] = pd.to_numeric(matched_atf['Transaction Amount'], errors='coerce').abs()
 			max_sort_keys = matched_atf.groupby(['Original Invoice', 'Temp_Amount'], dropna=False)['SortKey'].transform('max')
 			matched_atf.drop(columns=['Temp_Amount'], inplace=True)
